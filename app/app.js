9cfc06f698b2b48caec75abccf0fe4f633c0b97b
MrNVK <n.kalughniy@mail.ru>
2015-09-12 23:10:55 +0300

    Added rdsstore and n3 support

--- app/app.js
+++ app/app.js
@@ -1,13 +1,37 @@
 var express    = require('express'),
-	handlebars = require('express-handlebars'),
-	n3         = require('n3'),
-	app        = express();	
-	
+    handlebars = require('express-handlebars'),
+    rdfstore   = require('rdfstore'),
+    n3         = require('n3'),
+    fs         = require('fs'),
+    app        = express();	
+
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+var rdf = fs.readFileSync('app/rdf.n3').toString();
+
 app.get('/', function(req, res) {
-	res.render('index');
+  rdfstore.create(function(err, store) {
+    store.load('text/turtle', rdf, function(s,d) {
+      store.execute("SELECT * { ?s ?p ?o }", function(err, results) {
+        if (!err) {
+          var objects = [];
+          results.forEach(function(item) {
+            function getValue(complexString) {
+              return complexString.substr(complexString.indexOf('#') + 1);
+            }
+            
+            objects.push({s: getValue(item.s.value), 
+                          p: getValue(item.p.value), 
+                          o: getValue(item.o.value)});
+          });
+          console.log(objects);
+        }
+      });
+      
+    });
+  });
+  res.render('index');
 });
 	
 app.listen(3000);
