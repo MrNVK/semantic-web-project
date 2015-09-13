021889cfadfb93ff8243bc187eb2af10377059de
MrNVK <n.kalughniy@mail.ru>
2015-09-13 15:15:55 +0300

    Added pages index and details. Added bootstrap.

--- app/app.js
+++ app/app.js
@@ -1,36 +1,67 @@
 var express    = require('express'),
     handlebars = require('express-handlebars'),
     rdfstore   = require('rdfstore'),
-    n3         = require('n3'),
+    jsonld     = require('jsonld'),
     fs         = require('fs'),
     app        = express();	
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-var rdf = fs.readFileSync('app/rdf.n3').toString();
+var rdfJSON = JSON.parse(fs.readFileSync('app/json-ld.json').toString());
+
+function clearJson(jsonldArray) {
+  var results = [];
+  for (var i = 0; i < jsonldArray.length; i += 1) {
+    var node = jsonldArray[i];
+    var clearedObject = {};
+    for (var prop in node) {
+      var clearedProp = prop.substr(prop.indexOf('#') + 1);
+      if (prop[0] == '@') {
+        clearedProp = clearedProp.substr(1);
+      }
+      var clearedValue = "";
+      if (Array.isArray(node[prop])) {
+        clearedValue = clearJson(node[prop]);
+      } else if (typeof prop == 'string') {
+        clearedValue = node[prop].substr(node[prop].indexOf('#') + 1);
+      }
+      clearedObject[clearedProp] = clearedValue;
+    }
+    results.push(clearedObject);
+  }
+  return results;
+}
 
 app.get('/', function(req, res) {
-  rdfstore.create(function(err, store) {
-    store.load('text/turtle', rdf, function(s,d) {
-      store.execute("SELECT * { ?s ?p ?o }", function(err, results) {
-        if (!err) {
-          var objects = [];
-          results.forEach(function(item) {
-            function getValue(complexString) {
-              return complexString.substr(complexString.indexOf('#') + 1);
-            }
-            
-            objects.push({s: getValue(item.s.value), 
-                          p: getValue(item.p.value), 
-                          o: getValue(item.o.value)});
-          });
-          console.log(objects);
-        }
-      });
-    });
+  jsonld.expand(rdfJSON, function(err, results) {
+    var wrapper = {};
+    wrapper.processors = clearJson(results);
+    res.render('index', wrapper);
   });
-  res.render('index');
 });
-	
+
+function find(jsonArray, model, socket) {
+  var results = [];
+  for (var i = 0; i < jsonArray.length; i += 1) {
+    var node = jsonArray[i];
+    if (node.model[0].value == model && node.series[0].value == socket) {
+      results.push(node);
+    }
+  }
+  return results;
+}
+
+app.get('/reviews/:id', function(req, res) {
+  jsonld.expand(rdfJSON, function(err, results) {
+    var param = req.params.id;
+    var clearedJson = clearJson(results);
+    var splitedpRequest = param.split('_');
+
+    var findedJson = find(clearedJson, splitedpRequest[0], splitedpRequest[1]);
+    console.log(findedJson[0]);
+    res.render('reviews', findedJson[0]);
+  });
+});
+
 app.listen(3000);
